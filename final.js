8884ff341a06eb23b3f1f4dc8082140a495944f8
eltomirodro05 <tomasrodro12@gmail.com>
2025-04-10 12:47:29 -0500

    final.js

---
+++ final.js
@@ -0,0 +1,30 @@
+[
+  {
+    "_id": { "$oid": "67fabc1e502666d991882edf" },
+    "name": "entrenamientos",
+    "categoria": "juvenil",
+    "duracion": "2 horas"
+  },
+  {
+    "_id": { "$oid": "67fabd70502666d991882ee1" },
+    "name": "inscripciones",
+    "estado": "abiertas",
+    "fecha_inicio": "2024-05-01",
+    "fecha_cierre": "2024-06-01"
+  },
+  {
+    "_id": { "$oid": "67fabd78502666d991882ee2" },
+    "name": "jugadores",
+    "nombre": "Pedro Sánchez",
+    "edad": 15,
+    "posicion": "Shortstop",
+    "equipo": "Tiburones"
+  },
+  {
+    "_id": { "$oid": "67faf0396bcc830f8c32d039" },
+    "name": "entrenadores",
+    "nombre": "Miguel Torres",
+    "especialidad": "Lanzamiento",
+    "telefono": "0426-9876543"
+  }
+]
