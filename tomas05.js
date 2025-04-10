d710a3e3e45734ea2a22efd4121da22aad2477f3
eltomirodro05 <tomasrodro12@gmail.com>
2025-04-10 12:44:34 -0500

    tomas05.js

---
+++ tomas05.js
@@ -0,0 +1,22 @@
+db.otros.insertMany([
+  {
+    _id: ObjectId("67d3113e502666d991882edf"),
+    name: "horarios",
+    materia: "ingles"
+  },
+  {
+    _id: ObjectId("67d31170502666d991882ee1"),
+    name: "notas"
+  },
+  {
+    _id: ObjectId("67d31178502666d991882ee2"),
+    name: "Alumnos",
+    age: "30",
+    apellido: "rodriguez",
+    nombre: "luis"
+  },
+  {
+    _id: ObjectId("67dc3f396bcc830f8c32d039"),
+    name: "materias"
+  }
+])
